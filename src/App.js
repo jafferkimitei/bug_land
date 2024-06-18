af9b726e3e9552c2c32d92d660f84b8f0179bdb0
yunghavy <kimutaij192@gmail.com>
2024-06-18 16:48:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-800 flex flex-col justify-center items-center relative overflow-hidden">
+      <h1 className="text-white text-5xl font-bold mb-8">Welcome to Bug Land</h1>
+      <p className="text-white text-xl mb-16">
+        Watch the bugs crawl around as you enjoy the scenery.
+      </p>
+      <div className="absolute top-0 left-0 w-full h-full">
+        {[...Array(20)].map((_, i) => (
+          <div key={i} className="bug"></div>
+        ))}
+      </div>
     </div>
   );
 }
